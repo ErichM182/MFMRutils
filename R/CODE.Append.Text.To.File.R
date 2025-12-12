b7a9deaee27a102c07c7ce84e24960a543c0e66f
Erich MALETZKY <esmaletzky@gmail.com>
2025-12-12 16:25:04 +0200

    LIN-ArchHMB14 | 99.9% Completion of 'code.append.text.to.file()' !!!

--- R/CODE.Append.Text.To.File.R
+++ R/CODE.Append.Text.To.File.R
@@ -3,16 +3,28 @@
 #' @name code.append.text.to.file
 #' 
 #' @description
-#' The "SuiteMFMR" Null-Coalescing Operator (NCO) is similar to the "??" NCO of
-#' the DART Programming Language. The operator evaluates whether the "Left-Hand" R
-#' Object is NULL (in terms of its value or length or class <data type>) and returns
-#' the "Right-Hand" R Object if the "Left-Hand" Object is <indeed> NULL.
+#' Append text character vectors (strings) to specified file. This function was originally intended 
+#' to aide the "SuiteMFMR" R Library Developer Process (as part of the R Project init-phase). This
+#' is a standalone function and as such can also be used in (and by) other (3rd Party) R Projects.
 #'
-#' @param coLHO a dynamic (complex) object that captures the "Left-Hand" R Object
-#'              to be evaluated against the "NULL Criteria" (i.e. by way of value, 
-#'              length or class <data type> NULL Checks).
-#' @param coRHO a dynamic (complex) object that captures the "Right-Hand" R Object
-#'              (to be returned if the "Left-Hand" R Object is NULL).
+#' @param ssFilePath a charater vector (string/text) that defines the path where the file that needs
+#'                   to be appended to is located on the local directory.
+#' @param ssFileID a charater vector (string/text) that defines the file name (i.e. file identifier), 
+#'                 inclusive of the file extension) of the file that needs to be appended to.
+#' @param ssAppendText a charater vector (string) that defines the actual text (i.e. the character 
+#'                     vector or string) to be appended to the specified file.
+#' @param sbMultiAppend a logical (boolean) function argument that specifies whether multiple append
+#'                      actions are permitted (to the specified file) or not. When set to FALSE the 
+#'                      function checks whether the "ssAppendText" value already exists within the
+#'                      specified file and only appends if the text does not already exist (i.e. was
+#'                      not found) in the contents of the specified file. When set to TRUE the 
+#'                      function appends the "ssAppendText" value to the specified file irrespective
+#'                      of whether the text already exists in the specified file or not.
+#' @param sbPostPend a logical (boolean) function argument that specifies whether the "ssAppendText"
+#'                   value should be appended to the start or end of the existing (extant) specified
+#'                   file contents. When set to TRUE the "ssAppendText" value will be appended to 
+#                    the end; and when set FALSE the "ssAppendText" value will be append to the 
+#                    start of the existing content. 
 #'
 #' @examples
 #' ### Use the Null-Coalescing Operator (NCO) as follows: ...
@@ -27,9 +39,8 @@
 #' @export
 #? ### ### ###
 code.append.text.to.file <- function(
-    ssFilePath=".", ssFileID=".Rbuildignore", 
-    ssAppendText="^WIP$", sbMultiAppend=FALSE, sbPostPend=TRUE
-    
+  ssFilePath=".", ssFileID=".Rbuildignore", 
+  ssAppendText="^WIP$", sbMultiAppend=FALSE, sbPostPend=TRUE
 ) {
   
   
@@ -40,8 +51,9 @@ code.append.text.to.file <- function(
   
   
   
-  ####   STEP 02 - Define "Local Aliases" for Key Functions   ####
+  ####   STEP 02 - Define "Local Aliases" for ALL Internal Functions   ####
   # NOTES: This is a <NEW> approach to improve the R Session Memory Efficiency ...
+  rasBaseCAT        <- base::cat;
   rasBaseANY        <- base::any;
   rasBaseGREP       <- base::grep;
   rasBaseLENGTH     <- base::length;
@@ -51,7 +63,6 @@ code.append.text.to.file <- function(
   rasBaseReadLINES  <- base::readLines;
   rasBaseFilePATH   <- base::file.path;
   rasBaseCHARACTER  <- base::character;
-  rasBaseReadLINES  <- base::readLines;
   rasBaseDirCREATE  <- base::dir.create;
   rasBaseWriteLINES <- base::writeLines;
   rasBaseFileEXISTS <- base::file.exists;
@@ -69,66 +80,87 @@ code.append.text.to.file <- function(
   
   
   ####   STEP 04 - Create DIRECTORY & FILE IF-NOT-EXISTS   ####
-  rssIsNewlyCreatedFile_ <- FALSE;
+  rsbIsNewlyCreatedFile_ <- FALSE;
   rssFullPathFile_ <- rasBaseFilePATH(rssFilePath_, rssFileID_);
   if (!rasBaseFileEXISTS(rssFullPathFile_)) {   # <- If TRUE -> then file DOES NOT EXIST !!!
     
     ### 4.1 - Create required directory or directories (if not already exists) ...
     rasBaseDirCREATE(
-      path = rssFilePath_, recursive = T, showWarnings = F
+      path = rssFilePath_, recursive = TRUE, showWarnings = FALSE
     );
     
     ### 4.2 - Create the required File ...
-    rssIsNewlyCreatedFile_ <- TRUE;   # <- NB for STEP 5 (below) !!!
+    rsbIsNewlyCreatedFile_ <- TRUE;   # <- NB for STEP 5 (below) !!!
     rasBaseFileCREATE(rssFullPathFile_);
   }
   
   
   
   ####   STEP 05 - Append TEXT to File IF-TEXT-NOT-EXISTS   ####
-  rvsExtantFileContents_ <- rasBaseIfELSE(   # <- Extract existing (extant) file contents (NB) !!!
-    rssIsNewlyCreatedFile_, rasBaseCHARACTER(0),       # <- Returns a NULL Character R Object !!!
-    rasBaseReadLINES(rssFullPathFile_, warn = FALSE)   # <- Returns the actual contents of the file.
-  );
+  rvsExtantFileContents_ <- NULL;
+  rssNullSTRING_ <- rasBaseCHARACTER(0);
+  
+  ### Extract existing (extant) file contents (NB) !!!
+  if (rsbIsNewlyCreatedFile_) {
+    rvsExtantFileContents_ <- rssNullSTRING_;   # <- Returns a NULL Character R Object !!!
+  } else {
+    rvsExtantFileContents_ <- rasBaseReadLINES(
+      con = rssFullPathFile_, warn = FALSE
+    );   # <- Returns actual contents of file!!!
+  }
   
   rcoFileOUT_ <- NULL;
   if (rsbMultiAppend_) {   # <- Append Text (even if already exists in the file - multiple times)!!!
     if (rsbPostPend_) {    # <- Append to END of file (i.e. AFTER existing file contents) !!!
       rcoFileOUT_ <- rasBasePASTE0(
-        rvsExtantFileContents_, "\n", rssAppendText_
+        rasBasePASTE0(rvsExtantFileContents_, collapse = "\n"), "\n", rssAppendText_
       );
       rasBaseWriteLINES(
         text = c(rvsExtantFileContents_, rssAppendText_), con = rssFullPathFile_
       );
     } else {   # < Append to START of file (i.e. BEFORE existing file contents) !!!
       rcoFileOUT_ <- rasBasePASTE0(
-        rssAppendText_, "\n",rvsExtantFileContents_
+        rssAppendText_, "\n", rasBasePASTE0(rvsExtantFileContents_, collapse = "\n")
       );
       rasBaseWriteLINES(
         text = c(rssAppendText_, rvsExtantFileContents_), con = rssFullPathFile_
       );
     }
   } else {   # <- Append Text ONLY IF it does NOT ALREADY EXISTS (in the file - ONLY ONCE EVER) !!!
-    # Use grep to find if the exact pattern_to_add exists in any line
-    # We use fixed=TRUE because we are checking for the literal string "^WIP$",
-    # not whether the lines match the ^WIP$ pattern as a regex.
+    
+    rcoFileOUT_ <- rvsExtantFileContents_;
+    
+    ### Use grep to find if the exact pattern (^WIP$) exists in any line. We use fixed=TRUE because 
+    ### we are checking for the literal string "^WIP$", and not whether the lines match the ^WIP$ 
+    ### pattern as a regex.
     rsbAlreadyExistsTEXT_ <- rasBaseANY(
-      rasBaseGREP(pattern = rssAppendText_, x = rvsExtantFileContents_, fixed = TRUE)
+      rasBaseGREP(
+        pattern = rssAppendText_, 
+        x = rvsExtantFileContents_, fixed = TRUE
+      )
     );
     if (!rsbAlreadyExistsTEXT_) {   # <- If Text DOES NOT ALREADY EXISTS (in file) ...
       if (rsbPostPend_) {    # <- Append to END of file (i.e. AFTER existing file contents) !!!
         rcoFileOUT_ <- rasBasePASTE0(
-          rvsExtantFileContents_, "\n", rssAppendText_
+          rasBasePASTE0(rvsExtantFileContents_, collapse = "\n"), 
+          "\n", rssAppendText_
         );
         rasBaseWriteLINES(
-          text = c(rvsExtantFileContents_, rssAppendText_), con = rssFullPathFile_
+          con = rssFullPathFile_,
+          text = c(
+            rvsExtantFileContents_, rssAppendText_
+          )
         );
       } else {   # < Append to START of file (i.e. BEFORE existing file contents) !!!
         rcoFileOUT_ <- rasBasePASTE0(
-          rssAppendText_, "\n",rvsExtantFileContents_
+          rssAppendText_, "\n",
+          rasBasePASTE0(rvsExtantFileContents_, collapse = "\n") 
         );
         rasBaseWriteLINES(
-          text = c(rssAppendText_, rvsExtantFileContents_), con = rssFullPathFile_
+          con = rssFullPathFile_,
+          text = c(
+            rssAppendText_, rvsExtantFileContents_
+          )
         );
       }
     }
--- WIP/00_Helpers/rScratchPAD.R
+++ WIP/00_Helpers/rScratchPAD.R
@@ -1,12 +1,10 @@
 
 
-lstTEST_ <- MFMRutils:::devs.pull.libr.info()
-class(lstTEST_)
-lstTEST_$NAME
-lstTEST_[["VERSION"]]
-lstTEST_$VERS_STABLE
-lstTEST_$VERS_ALPHA
-
 rcoRbuildIgnoreTEST_ <- MFMRutils::code.append.text.to.file(
-  
+  ssAppendText = "^WIP$", sbMultiAppend = T, sbPostPend = F
 )
+base::cat(rcoRbuildIgnoreTEST_)
+
+
+
+base::readLines(con = "./.Rbuildignore", warn = FALSE)
